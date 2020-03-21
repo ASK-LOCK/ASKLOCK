b9e077a824e717804ae2b63b640f8516495ca8fb
Aaron Geronimo <42796623+aaronjgeronimo@users.noreply.github.com>
2020-03-20 19:49:30 -1000

    big update

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={8}>
+          <Grid.Column width={6}>
             <a href="/signin#/signin">
-            <img src="/images/lock_image.png"/>
+              <img className="animated bounceInUp" src="/images/lock_image_transparent_circle.png"/>
             </a>
-            <p>We will sell your password information to make money!</p>
           </Grid.Column>
-
         </Grid>
     );
   }
