38b152d38827674d533c73af575234ee216c51a2
Aaron Geronimo <42796623+aaronjgeronimo@users.noreply.github.com>
2020-03-20 19:21:19 -1000

    Update landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <div className="vertical-placement">
+        <Grid verticalAlign='middle' textAlign='center' container>
+          <Grid.Column width={6}>
             <a href="/signin#/signin">
-            <img className="center-image animated bounceInUp" src="/images/lock_image_transparent_circle.png"/>
+              <img className="animated bounceInUp" src="/images/lock_image_transparent_circle.png"/>
             </a>
-        </div>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
